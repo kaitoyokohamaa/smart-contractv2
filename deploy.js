9bb308d4fad50074b014449890aacf79dba3417b
kaitoyokohamaa <s1A121700885@toyo.jp>
2022-09-17 15:42:26 +0900

    feat: make first deploy

--- deploy.js
+++ deploy.js
@@ -7,13 +7,11 @@ const dotenv = require("dotenv");
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 const mnemonicPhrase = process.env.MNEMONIC_PHRASE;
 const BASEAPI = process.env.BASEAPI;
-console.log(mnemonicPhrase);
 const provider = new HDWalletProvider(mnemonicPhrase, BASEAPI);
 
 const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  console.log("Attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
@@ -21,6 +19,5 @@ const deploy = async () => {
       arguments: ["Hi there!"],
     })
     .send({ gas: "1000000", from: accounts[0] });
-  console.log("Contract deployed to", result.options.address);
 };
 deploy();
