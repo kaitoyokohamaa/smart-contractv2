610d704cab4a2be80439d151766a8a6e13715a16
kaitoyokohamaa <s1A121700885@toyo.jp>
2022-09-17 15:51:38 +0900

    feat: finish first contract

---
+++ inbox-updated/deploy.js
@@ -0,0 +1,23 @@
+// deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+const mnemonicPhrase = process.env.MNEMONIC_PHRASE;
+const BASEAPI = process.env.BASEAPI;
+const provider = new HDWalletProvider(mnemonicPhrase, BASEAPI);
+
+const web3 = new Web3(provider);
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ gas: "1000000", from: accounts[0] });
+};
+deploy();
